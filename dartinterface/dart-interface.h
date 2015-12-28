c22bae5d0978a29088d9cb9eace705305ca7384e
Jiaji Zhou <robinzhou55@gmail.com>
2015-12-28 14:51:18 -0500

    merge

--- dartinterface/dart-interface.h
+++ dartinterface/dart-interface.h
@@ -1,6 +1,6 @@
 #include "env-model.h"
 // Minimum interface between outside trajectory optimizer and dart environment. 
-// Think of this interface provides only the mathematical quantities, being agnostic to the fact that it's optimizing through contact. All the distance computation logic, contact information/jacobian should be hide away from the optimizer. 
+// Think of this interface provides only the mathematical quantities, being agnostic to the fact that it's optimizing through contact. All the distance computation logic, contact information/jacobian should be hidden away from the optimizer. 
 
 class DartInterface {
  public:
